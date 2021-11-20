e83562ffe46b0cbdd70199ec3066174764627782
yyifan-Onyen <yyifan@ad.unc.edu>
2021-11-20 17:04:03 -0500

    a

--- server.js
+++ server.js
@@ -34,19 +34,19 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
-app.get("/app/users/:id", (req, res) => {	
+app.get("/app/user	/:id", (req, res) => {	
 	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").run(req.params.id);
 	res.status(200).json(stmt);
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {	
-	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").run(req.params.id);
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").run(req.body.user, req.body.pass, req.params.id);
 	res.status(200).json({"message":"1 record updated: ID 2 (200)"})
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {	
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?").run(req.params.id);
-	res.status(200).json({"message":"1 record updated: ID 2 (200)"})
+	res.status(200).json({"message":"1 record deleted: ID 2 (200)"})
 });
 // Default response for any other request
 app.use(function(req, res){
