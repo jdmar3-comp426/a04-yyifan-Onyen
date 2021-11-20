98f7e7483be4dcc3af4589fdbe5753e1d83723a7
yyifan-Onyen <yyifan@ad.unc.edu>
2021-11-20 17:10:14 -0500

    a

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get("/app/users", (req, res) => {
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/user/:id", (req, res) => {	
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").run(req.params.id);
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get(req.params.id);
 	res.status(200).json(stmt);
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
