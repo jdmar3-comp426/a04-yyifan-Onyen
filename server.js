70bc3a217a58c68b9132b98668fce82daf52131f
yyifan-Onyen <yyifan@ad.unc.edu>
2021-11-27 17:55:34 -0500

    d

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.get("/app/", (req, res, next) => {
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new", (req, res) => {	
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)").run(req.body.user,md5(req.body.pass));
-	res.status(200).json({"message":"1 record createed: ID 3 (201)"});
-	
+	res.status(200).json({"message":"1 record created: ID 3 (201)"});
+
 });
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
