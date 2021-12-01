5155def01bee9816569a897cc52eed3d84e0afd9
yyifan-Onyen <yyifan@ad.unc.edu>
2021-12-01 17:08:18 -0500

    a04 really hard

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/app/", (req, res, next) => {
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new", (req, res) => {	
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)").run(req.body.user,md5(req.body.pass));
-	res.status(201).json({"message":"1 record created: ID 3 (201)"});
+	res.status(200).json({"message":"1 record created: ID 3 (201)"});
 
 });
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
